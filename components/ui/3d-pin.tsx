84ba1376e43a07910858cef21a76ea3d6cf9d8f1
Hamed-Ajaj <hamedsgaming@gmail.com>
2024-11-20 11:04:15 +0200

    links fixed

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -36,25 +36,25 @@ export const PinContainer = ({
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      {/* <a href={href} target="_blank"> */}
-      <div
-        style={{
-          perspective: "1000px",
-          transform: "rotateX(70deg) translateZ(0deg)",
-        }}
-        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
-      >
+      <a href={href} target="_blank">
         <div
           style={{
-            transform: transform,
+            perspective: "1000px",
+            transform: "rotateX(70deg) translateZ(0deg)",
           }}
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)]  border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
         >
-          <div className={cn(" relative z-50 ", className)}>{children}</div>
+          <div
+            style={{
+              transform: transform,
+            }}
+            className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)]  border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          >
+            <div className={cn(" relative z-50 ", className)}>{children}</div>
+          </div>
         </div>
-      </div>
-      <PinPerspective title={title} href={href} />
-      {/* </a> */}
+        <PinPerspective title={title} href={href} />
+      </a>
     </div>
   );
 };
